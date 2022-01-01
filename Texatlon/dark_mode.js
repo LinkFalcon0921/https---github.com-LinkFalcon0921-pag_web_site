329a2deac47235ec95cc2f9061c884ecca8420b2
LinkFalcon0921 <elioerickramos@gmail.com>
2022-01-01 17:04:02 -0400

    first commit

---
+++ Texatlon/dark_mode.js
@@ -0,0 +1,27 @@
+function into_dark() {
+  const docs = document.getElementsByTagName("div");
+  const tagClass = "dark-mode";
+
+//   Body color
+  document.body.classList.toggle(tagClass);
+
+  for (let index = 0; index < docs.length; index++) {
+    const tr = docs.item(index);
+  }
+//   Functions to other elements
+    dark_nav_space()
+    dark_icon_mode()
+}
+
+function dark_nav_space(){
+   document.getElementById("nv").classList.toggle("dark-navbar");
+   document.getElementById("nv").classList.toggle("navbar");
+
+}
+
+function dark_icon_mode(){
+  const elem = document.getElementById("dark_icon").children[0];
+  // console.log(elem)
+  elem.classList.toggle("fa-sun");
+  elem.classList.toggle("fa-moon");
+}
